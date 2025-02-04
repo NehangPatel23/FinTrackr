3241460c9767dcbf6576e369fcfe428a36edbd2a
Nehang Patel <90214774+NehangPatel23@users.noreply.github.com>
2025-02-03 19:48:15 -0500

    tax page edited

--- Code/lib/screens/taxes/taxes_page.dart
+++ Code/lib/screens/taxes/taxes_page.dart
@@ -12,6 +12,8 @@ class _TaxesPageState extends State<TaxesPage> {
   TextEditingController incomeController = TextEditingController();
   TextEditingController dependentsController = TextEditingController();
   String? selectedState;
+  String? selectedCountry;
+  String? maritalStatus;
 
   final List<String> states = [
     "Alabama",
@@ -66,104 +68,251 @@ class _TaxesPageState extends State<TaxesPage> {
     "Wyoming"
   ];
 
+  final List<String> countries = [
+    "United States",
+    "Canada",
+    "United Kingdom",
+    "Germany",
+    "France",
+    "Australia",
+    "India",
+    "China",
+    "Japan",
+    "South Korea",
+    "Brazil",
+    "Mexico",
+    "Italy",
+    "Spain",
+    "Netherlands",
+    "Switzerland",
+    "Sweden",
+    "Norway",
+    "Denmark",
+    "Finland",
+    "Ireland",
+    "New Zealand",
+    "Singapore",
+    "South Africa"
+  ];
+
+  void _showInfoDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Got it"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showCalculateTaxesPopup() {
+    _showInfoDialog(
+        "Calculation", "Tax calculation logic will be implemented here.");
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Sort the states and countries alphabetically
+    states.sort();
+    countries.sort();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.surface,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                '/Users/nehangpatel/Downloads/FinTrackrApp/Code/assets/tax.png',
-                height: 150,
-                width: 150,
-              ),
-              const Text(
-                'Calculate Your Taxes',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                'What is your income?',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: TextFormField(
-                  controller: incomeController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(
-                      FontAwesomeIcons.dollarSign,
-                      size: 18,
-                      color: Colors.grey.shade500,
-                    ),
-                    border: OutlineInputBorder(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/tax.png',
+                  height: 150,
+                  width: 150,
+                ),
+                const Text(
+                  'Calculate Your Taxes',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 32),
+
+                _buildQuestionRow('What is your income?', "Income Information",
+                    "Please input your total annual income before any deductions."),
+                _buildTextField(incomeController, FontAwesomeIcons.dollarSign),
+
+                const SizedBox(height: 30),
+
+                _buildQuestionRow(
+                    'What is your marital status?',
+                    "Marital Status Information",
+                    "Please select your marital status."),
+                _buildMaritalStatusRadio(),
+
+                const SizedBox(height: 30),
+
+                _buildQuestionRow(
+                    'How many dependents do you have?',
+                    "Dependents Information",
+                    "Please enter the number of dependents you are financially responsible for."),
+                _buildTextField(
+                    dependentsController, FontAwesomeIcons.peopleGroup),
+
+                const SizedBox(height: 30),
+
+                _buildQuestionRow('What state are you from?', "State Selection",
+                    "Please select your state of residence."),
+                _buildDropdown(states, selectedState, (value) {
+                  setState(() => selectedState = value);
+                }),
+
+                const SizedBox(height: 30),
+
+                _buildQuestionRow(
+                    'What country are you from?',
+                    "Country Selection",
+                    "Please select your country of residence."),
+                _buildDropdown(countries, selectedCountry, (value) {
+                  setState(() => selectedCountry = value);
+                }),
+
+                const SizedBox(height: 40),
+
+                /// Submit Button with Gradient
+                GestureDetector(
+                  onTap: _showCalculateTaxesPopup,
+                  child: Container(
+                    width: 200,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Theme.of(context).colorScheme.tertiary,
+                          Theme.of(context).colorScheme.secondary,
+                          Theme.of(context).colorScheme.primary,
+                        ],
+                      ),
                       borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 40),
-              const Text(
-                'How many dependents do you have?',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: TextFormField(
-                  controller: dependentsController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    prefixIcon: Icon(
-                      FontAwesomeIcons.peopleGroup,
-                      size: 18,
-                      color: Colors.grey.shade500,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              const Text(
-                'What state are you from?',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: DropdownMenu<String>(
-                  initialSelection: selectedState,
-                  dropdownMenuEntries: states
-                      .map((state) =>
-                          DropdownMenuEntry(value: state, label: state))
-                      .toList(),
-                  onSelected: (value) {
-                    setState(() {
-                      selectedState = value;
-                    });
-                  },
-                ),
-              ),
-            ],
+
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  /// Helper method to build text fields
+  Widget _buildTextField(TextEditingController controller, IconData icon) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: TextFormField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          prefixIcon: Icon(icon, size: 18, color: Colors.grey.shade500),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Helper method to build marital status radio buttons
+  Widget _buildMaritalStatusRadio() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: ['Single', 'Married', 'Divorced']
+          .map((status) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    Radio<String>(
+                      value: status,
+                      groupValue: maritalStatus,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          maritalStatus = newValue;
+                        });
+                      },
+                    ),
+                    Text(status, style: const TextStyle(fontSize: 16)),
+                  ],
+                ),
+              ))
+          .toList(),
+    );
+  }
+
+  /// Helper method to build dropdowns
+  Widget _buildDropdown(List<String> items, String? selectedValue,
+      ValueChanged<String?> onChanged) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: DropdownButtonHideUnderline(
+        child: DropdownButtonFormField<String>(
+          value: selectedValue,
+          hint: const Text("Select an option"),
+          isDense: true,
+          menuMaxHeight: 250,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide.none,
+            ),
+          ),
+          items: items.map((String item) {
+            return DropdownMenuItem<String>(
+              value: item,
+              child: Text(item, textAlign: TextAlign.center),
+            );
+          }).toList(),
+          onChanged: onChanged,
+        ),
+      ),
+    );
+  }
+
+  /// Helper method to build question rows with icons
+  Widget _buildQuestionRow(String question, String title, String message) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(question,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+        IconButton(
+          icon: Icon(FontAwesomeIcons.circleInfo,
+              size: 15, color: Colors.blue.shade700),
+          onPressed: () => _showInfoDialog(title, message),
+        ),
+      ],
     );
   }
 }
